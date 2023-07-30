4b2a5d144d5df4c8bd4b3536b7feacc55f0586f9
Emon Hossian <emondd4@gmail.com>
2023-07-31 00:19:44 +0600

    everything done

--- lib/SpotifyDashboardScreen.dart
+++ lib/SpotifyDashboardScreen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:spotify_ui_challenge/SpofityPlayScreen.dart';
+import 'package:spotify_ui_challenge/util/any_widget_marquee.dart';
 
 class SpotifyDashboardScreen extends StatefulWidget {
   const SpotifyDashboardScreen({super.key});
@@ -17,115 +19,186 @@ class _SpotifyDashboardScreenState extends State<SpotifyDashboardScreen> {
           height: MediaQuery.sizeOf(context).height,
           width: MediaQuery.sizeOf(context).width,
           color: Colors.black,
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(10.0,30.0,10.0,10.0),
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Stack(
+            children: [
+              Positioned(top:0,bottom:0,left:0,right:0,child: Padding(
+                padding: const EdgeInsets.fromLTRB(10.0,30.0,10.0,10.0),
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      const Row(
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          Text("Good evening",
-                              style: TextStyle(
-                                  fontFamily: 'Gotham',
-                                  fontSize: 22.0,
-                                  fontWeight: FontWeight.bold,
-                                  letterSpacing: 0,
-                                  color: Colors.white)),
+                          const Row(
+                            children: [
+                              Text("Good evening",
+                                  style: TextStyle(
+                                      fontFamily: 'Gotham',
+                                      fontSize: 22.0,
+                                      fontWeight: FontWeight.bold,
+                                      letterSpacing: 0,
+                                      color: Colors.white)),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              SvgPicture.asset("assets/notification.svg",height: 28.0,width: 28.0,color: Colors.white,),
+                              const SizedBox(width: 20.0,),
+                              SvgPicture.asset("assets/clock.svg",height: 28.0,width: 28.0,color: Colors.white,),
+                              const SizedBox(width: 20.0,),
+                              SvgPicture.asset("assets/settings.svg",height: 28.0,width: 28.0,color: Colors.white,),
+                            ],
+                          )
                         ],
                       ),
-                      Row(
+                      const SizedBox(height: 30.0,),
+                      Wrap(
+                        direction: Axis.horizontal,
+                        spacing: 10.0,
+                        runSpacing: 10.0,
                         children: [
-                          SvgPicture.asset("assets/notification.svg",height: 28.0,width: 28.0,color: Colors.white,),
-                          const SizedBox(width: 20.0,),
-                          SvgPicture.asset("assets/clock.svg",height: 28.0,width: 28.0,color: Colors.white,),
-                          const SizedBox(width: 20.0,),
-                          SvgPicture.asset("assets/settings.svg",height: 28.0,width: 28.0,color: Colors.white,),
+                          _likedSongs(),
+                          _normalSongs("assets/la_lecon.jpg","La Lecon Particuliere"),
+                          _normalSongs("assets/moments.jpg","Moments"),
+                          _normalSongs("assets/yad_oud.jpg","Yad Oud"),
+                          _normalSongs("assets/mayerling.jpg","Mayerling"),
+                          _normalSongs("assets/sufficient.jpg","Suffice"),
                         ],
+                      ),
+                      const SizedBox(height: 40.0,),
+                      const Text("Popular Radio",
+                          style: TextStyle(
+                              fontFamily: 'Gotham',
+                              fontSize: 22.0,
+                              fontWeight: FontWeight.bold,
+                              letterSpacing: 0,
+                              color: Colors.white)),
+                      const SizedBox(height: 20.0,),
+                      SizedBox(
+                          height: 220,
+                          child: ListView.builder(itemBuilder: (context,index) => _listItem("assets/album.jpg",0,""), itemCount: 7, scrollDirection: Axis.horizontal,)
+                      ),
+                      const Text("Recommended Stations",
+                          style: TextStyle(
+                              fontFamily: 'Gotham',
+                              fontSize: 22.0,
+                              fontWeight: FontWeight.bold,
+                              letterSpacing: 0,
+                              color: Colors.white)),
+                      const SizedBox(height: 20.0,),
+                      SizedBox(
+                          height: 220,
+                          child: ListView.builder(itemBuilder: (context,index) => _listItem("assets/station.jpg",0,""), itemCount: 7, scrollDirection: Axis.horizontal,)
+                      ),
+                      const Text("Recently Played",
+                          style: TextStyle(
+                              fontFamily: 'Gotham',
+                              fontSize: 22.0,
+                              fontWeight: FontWeight.bold,
+                              letterSpacing: 0,
+                              color: Colors.white)),
+                      const SizedBox(height: 20.0,),
+                      SizedBox(
+                          height: 170,
+                          child: ListView.builder(itemBuilder: (context,index) => _recentPlay("assets/sufficient.jpg"), itemCount: 7, scrollDirection: Axis.horizontal,)
+                      ),
+                      const Text("Best of artists",
+                          style: TextStyle(
+                              fontFamily: 'Gotham',
+                              fontSize: 22.0,
+                              fontWeight: FontWeight.bold,
+                              letterSpacing: 0,
+                              color: Colors.white)),
+                      const SizedBox(height: 20.0,),
+                      SizedBox(
+                          height: 220,
+                          child: ListView.builder(itemBuilder: (context,index) => _listItem("assets/artist.png",1,"The Weekend"), itemCount: 7, scrollDirection: Axis.horizontal,)
+                      ),
+                      const Text("Best of artists",
+                          style: TextStyle(
+                              fontFamily: 'Gotham',
+                              fontSize: 22.0,
+                              fontWeight: FontWeight.bold,
+                              letterSpacing: 0,
+                              color: Colors.white)),
+                      const SizedBox(height: 20.0,),
+                      SizedBox(
+                          height: 220,
+                          child: ListView.builder(itemBuilder: (context,index) => _listItem("assets/daily.jpg",0,""), itemCount: 7, scrollDirection: Axis.horizontal,)
+                      ),
+                    ],
+                  ),
+                ),
+              )),
+              Positioned(bottom: 20.0,child: InkWell(
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (_) => const SpotifyPlayPage()));
+                },
+                child: Container(
+                  width: MediaQuery.sizeOf(context).width,
+                  height: 60.0,
+                  decoration: BoxDecoration(
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.black,
+                        blurRadius: 15.0, // soften the shadow
+                        spreadRadius: 5.0, //extend the shadow
+                        offset: Offset(
+                          5.0, // Move to right 5  horizontally
+                          5.0, // Move to bottom 5 Vertically
+                        ),
                       )
                     ],
+                    borderRadius: BorderRadius.circular(10.0),
+                    color: const Color(0xff484129),
                   ),
-                  const SizedBox(height: 30.0,),
-                  Wrap(
-                    direction: Axis.horizontal,
-                    spacing: 10.0,
-                    runSpacing: 10.0,
-                    children: [
-                      _likedSongs(),
-                      _normalSongs("assets/la_lecon.jpg","La Lecon Particuliere"),
-                      _normalSongs("assets/moments.jpg","Moments"),
-                      _normalSongs("assets/yad_oud.jpg","Yad Oud"),
-                      _normalSongs("assets/mayerling.jpg","Mayerling"),
-                      _normalSongs("assets/sufficient.jpg","Suffice"),
-                    ],
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Row(
+                      children: [
+                        Container(
+                          height: 42.0,
+                          width: 42.0,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(5.0),
+                            image: const DecorationImage(image: AssetImage("assets/album_art.jpg")),
+                          ),
+                        ),
+                        const SizedBox(width: 10.0,),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              AnyMargueeWidget(marginLeft: 10.0, betweenSpacing: 100.0, width: 220.0, height: 20.0, child: const Text("TEYA DORA - DŽANUM",
+                                  style: TextStyle(
+                                      fontFamily: 'Gotham',
+                                      fontSize: 14.0,
+                                      fontWeight: FontWeight.bold,
+                                      letterSpacing: 0,
+                                      color: Colors.white))),
+                              Text("TEYA DORA",
+                                  style: TextStyle(
+                                      fontFamily: 'Gotham',
+                                      fontSize: 14.0,
+                                      fontWeight: FontWeight.bold,
+                                      letterSpacing: 0,
+                                      color: Colors.white54))
+                            ],
+                          ),
+                        ),
+                        const SizedBox(width: 5.0,),
+                        SvgPicture.asset("assets/device.svg",height: 28.0,width: 28.0,color: Colors.white,),
+                        const SizedBox(width: 15.0,),
+                        const Icon(Icons.favorite,color: Color(0xff00d963),size: 28.0,),
+                        const SizedBox(width: 10.0,),
+                        const Icon(Icons.play_arrow_sharp,size: 32.0,color: Colors.white,)
+                      ],
+                    ),
                   ),
-                  const SizedBox(height: 40.0,),
-                  const Text("Popular Radio",
-                      style: TextStyle(
-                          fontFamily: 'Gotham',
-                          fontSize: 22.0,
-                          fontWeight: FontWeight.bold,
-                          letterSpacing: 0,
-                          color: Colors.white)),
-                  const SizedBox(height: 20.0,),
-                  SizedBox(
-                    height: 220,
-                      child: ListView.builder(itemBuilder: (context,index) => _listItem("assets/album.jpg",0,""), itemCount: 7, scrollDirection: Axis.horizontal,)
-                  ),
-                  const Text("Recommended Stations",
-                      style: TextStyle(
-                          fontFamily: 'Gotham',
-                          fontSize: 22.0,
-                          fontWeight: FontWeight.bold,
-                          letterSpacing: 0,
-                          color: Colors.white)),
-                  const SizedBox(height: 20.0,),
-                  SizedBox(
-                      height: 220,
-                      child: ListView.builder(itemBuilder: (context,index) => _listItem("assets/station.jpg",0,""), itemCount: 7, scrollDirection: Axis.horizontal,)
-                  ),
-                  const Text("Recently Played",
-                      style: TextStyle(
-                          fontFamily: 'Gotham',
-                          fontSize: 22.0,
-                          fontWeight: FontWeight.bold,
-                          letterSpacing: 0,
-                          color: Colors.white)),
-                  const SizedBox(height: 20.0,),
-                  SizedBox(
-                      height: 170,
-                      child: ListView.builder(itemBuilder: (context,index) => _recentPlay("assets/sufficient.jpg"), itemCount: 7, scrollDirection: Axis.horizontal,)
-                  ),
-                  const Text("Best of artists",
-                      style: TextStyle(
-                          fontFamily: 'Gotham',
-                          fontSize: 22.0,
-                          fontWeight: FontWeight.bold,
-                          letterSpacing: 0,
-                          color: Colors.white)),
-                  const SizedBox(height: 20.0,),
-                  SizedBox(
-                      height: 220,
-                      child: ListView.builder(itemBuilder: (context,index) => _listItem("assets/artist.png",1,"The Weekend"), itemCount: 7, scrollDirection: Axis.horizontal,)
-                  ),
-                  const Text("Best of artists",
-                      style: TextStyle(
-                          fontFamily: 'Gotham',
-                          fontSize: 22.0,
-                          fontWeight: FontWeight.bold,
-                          letterSpacing: 0,
-                          color: Colors.white)),
-                  const SizedBox(height: 20.0,),
-                  SizedBox(
-                      height: 220,
-                      child: ListView.builder(itemBuilder: (context,index) => _listItem("assets/daily.jpg",0,""), itemCount: 7, scrollDirection: Axis.horizontal,)
-                  ),
-                ],
-              ),
-            ),
+                ),
+              ),),
+            ],
           ),
         ),
       ),
---
+++ lib/util/any_widget_marquee.dart
@@ -0,0 +1,122 @@
+library any_widget_marquee;
+
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class AnyMargueeSpeed {
+  static const double NORMAL_SPEED = 150.0;
+}
+
+class AnyMargueeWidget extends StatefulWidget {
+  AnyMargueeWidget({
+    required this.child,
+    Key? key,
+    required this.marginLeft,
+    required this.betweenSpacing,
+    required this.width,
+    required this.height,
+    this.speedRate = 1,
+    this.scrollFromEnd = true,
+    this.delayedStart = const Duration(seconds: 0),
+  }) : super(key: key);
+
+  double marginLeft;
+  double betweenSpacing;
+  final Widget child;
+  double? width;
+  double? height;
+  final double speedRate;
+  bool scrollFromEnd;
+  Duration delayedStart;
+
+  @override
+  _AnyMargueeWidgetState createState() => _AnyMargueeWidgetState();
+}
+
+class _AnyMargueeWidgetState extends State<AnyMargueeWidget> {
+  late Timer _anyMargueeTimer;
+  late ScrollController _scrollController;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) async {
+      await Future.delayed(widget.delayedStart);
+      startTimer();
+    });
+  }
+
+  void startTimer() {
+    _anyMargueeTimer = Timer.periodic(Duration(microseconds: 16), (timer) {
+      if (_scrollController.hasClients) {
+        final distance = _scrollController.offset ?? 0;
+        _scrollController.jumpTo(distance + (1 / AnyMargueeSpeed.NORMAL_SPEED) * widget.speedRate);
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ScrollNotificationInterceptor(
+      key: null,
+      child: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          final biggestWidth = constraints.biggest.width;
+          final biggestHeight = constraints.biggest.height;
+          widget.marginLeft ??= biggestWidth;
+          widget.betweenSpacing ??= biggestWidth;
+          widget.width ??= biggestWidth;
+          widget.height ??= biggestHeight;
+
+          return Container(
+            width: widget.width,
+            height: widget.height,
+            alignment: Alignment.center,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: double.maxFinite.toInt(),
+              itemBuilder: (context, index) {
+                final distance = (index == 0 ? widget.marginLeft : widget.betweenSpacing);
+                return Container(
+                  padding: EdgeInsets.only(left: widget.scrollFromEnd ? distance : 0),
+                  alignment: Alignment.center,
+                  child: widget.child,
+                );
+              },
+              controller: _scrollController,
+              physics: NeverScrollableScrollPhysics(),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _anyMargueeTimer.cancel();
+    super.dispose();
+    _scrollController.dispose();
+  }
+}
+
+class ScrollNotificationInterceptor extends StatelessWidget {
+  final Widget child;
+
+  ScrollNotificationInterceptor({
+    Key? key,
+    required this.child,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return NotificationListener(
+      onNotification: (ScrollNotification notification) {
+        return true;
+      },
+      child: this.child,
+    );
+  }
+}
